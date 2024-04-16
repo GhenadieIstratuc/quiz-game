319775bf5fc19d8f61942ba77da9a41d8d59b697
Ghenadie Istratuc <ghenadie.istratuc@gmail.com>
2024-04-16 12:38:33 +0300

    Initial commit

---
+++ data.py
@@ -0,0 +1,46 @@
+question_data = [
+    {"category": "Geography",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "A group of islands is called an archipelago.",
+     "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "The Republic of Malta is the smallest micro-state worldwide.",
+     "correct_answer": "False", "incorrect_answers": ["True"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Vatican City is a country.", "correct_answer": "True",
+     "incorrect_answers": ["False"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Alaska is the largest state in the United States.",
+     "correct_answer": "True", "incorrect_answers": ["False"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Toronto is the capital city of the North American country of Canada.",
+     "correct_answer": "False", "incorrect_answers": ["True"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Hungary is the only country in the world beginning with H.",
+     "correct_answer": "False", "incorrect_answers": ["True"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "New Haven is the capital city of the state of Connecticut in the United States.",
+     "correct_answer": "False", "incorrect_answers": ["True"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Nova Scotia is on the east coast of Canada.",
+     "correct_answer": "True", "incorrect_answers": ["False"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "Rhode Island is actually located on the US mainland, despite its name.",
+     "correct_answer": "True", "incorrect_answers": ["False"]
+     },
+    {"category": "Geography", "type": "boolean", "difficulty": "easy",
+     "question": "There is an island in Japan called Kunoshima, A.K.A. Rabbit Island, "
+                 "so named because of it's huge population of rabbits.",
+     "correct_answer": "True", "incorrect_answers": ["False"]
+     }
+]
